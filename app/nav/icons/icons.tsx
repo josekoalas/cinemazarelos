314efd9b8cc73ab2dd5b12638fe794367c87b42c
josekoalas <jose.pazos.perez@rai.usc.es>
2022-12-19 14:35:08 +0100

    Iconos 🦋

---
+++ app/nav/icons/icons.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import InstagramIcon from "./instagram.svg";
+import FilmAffinityIcon from "./filmaffinity.svg";
+import LetterboxdIcon from "./letterboxd.svg";
+
+export { InstagramIcon, FilmAffinityIcon, LetterboxdIcon };
--- next.config.js
+++ next.config.js
@@ -2,8 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  
   experimental: {
     appDir: true,
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
   },
 }
 
